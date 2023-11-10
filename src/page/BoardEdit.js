253a06440aab362279ad852859d6007365c23708
LeeJungHoon <gns14585@naver.com>
2023-11-10 09:37:47 +0900

    수정 버튼 클릭시 수정 화면("/edit/:id)으로 이동

--- src/page/BoardEdit.js
+++ src/page/BoardEdit.js
@@ -1,18 +1,21 @@
 import {
   Box,
+  Button,
   FormControl,
   FormLabel,
   Input,
   Spinner,
   Textarea,
 } from "@chakra-ui/react";
-import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { useImmer } from "use-immer";
 import axios from "axios";
 
 export function BoardEdit() {
   const [board, updateBoard] = useImmer(null);
+  const navigate = useNavigate();
+
 
   // /edit/:id
   const { id } = useParams();
@@ -27,23 +30,33 @@ export function BoardEdit() {
     return <Spinner />;
   }
 
+
+
   return (
     <Box>
       <h1>{id}번 글 수정</h1>
       <FormControl>
         <FormLabel>제목</FormLabel>
-        <Input value={board.title} />
+        <Input value={board.title} onChange={handleTitleChange} />
       </FormControl>
 
       <FormControl>
         <FormLabel>본문</FormLabel>
-        <Textarea value={board.content} />
+        <Textarea
+          value={board.content}
+          onChange={}
+        />
       </FormControl>
 
       <FormControl>
         <FormLabel>작성자</FormLabel>
-        <Input value={board.writer} />
+        <Input
+          value={board.writer}
+          onChange={}
+        />
       </FormControl>
+
+      <Button onClick={() => navigate("/")}>수정완료</Button>
     </Box>
   );
 }
