53eb77cef137522e350df7a141816cbe81806dfb
이정훈 <gns14585@naver.com>
2023-11-14 15:16:21 +0900

    로그인, 로그아웃 기능

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,14 +1,25 @@
-import { Button, Flex } from "@chakra-ui/react";
+import {Button, Flex, useToast} from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import {useContext} from "react";
+import {LoginContext} from "../App";
 
 export function NavBar() {
+  const {fetchLogin, login, isAuthenticated} = useContext(LoginContext)
+  const toast = useToast();
   const navigate = useNavigate();
 
   function handleLogOut() {
     // TODO : 로그아웃 후 할 일 추가
     axios.post("/api/member/logout")
-      .then(() => console.log("로그아웃 성공"));
+      .then(() => {
+        toast({
+          description: "로그아웃 되었습니다.",
+          status: "info",
+        })
+        navigate("/");
+      })
+      .finally(() => fetchLogin());
   }
 
   return (
