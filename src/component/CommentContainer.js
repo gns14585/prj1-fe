75f3f6b02d2d94b91e4a49b75e57a1126d2340c3
LeeJungHoon <gns14585@naver.com>
2023-11-16 09:36:48 +0900

    저장 버튼 클릭시 수정된 댓글과, 댓글 아이디 put / api/comment/edit로 전송

--- src/component/CommentContainer.js
+++ src/component/CommentContainer.js
@@ -48,6 +48,14 @@ function CommentItem({ comment, onDeleteModalOpen }) {
 
   const { hasAccess } = useContext(LoginContext);
 
+  function handleSubmit() {
+    axios
+      .put("/api/comment/edit", { id: comment.id, comment: commentEdited })
+      .then(() => console.log("good"))
+      .catch(() => console.log("bad"))
+      .finally(() => console.log("done"));
+  }
+
   return (
     <Box>
       <Flex justifyContent="space-between">
@@ -61,10 +69,15 @@ function CommentItem({ comment, onDeleteModalOpen }) {
           </Text>
 
           {isEditing && (
-            <Textarea
-              value={commentEdited}
-              onChange={(e) => setCommentEdited(e.target.value)}
-            />
+            <Box>
+              <Textarea
+                value={commentEdited}
+                onChange={(e) => setCommentEdited(e.target.value)}
+              />
+              <Button colorScheme="blue" onClick={handleSubmit}>
+                저장
+              </Button>
+            </Box>
           )}
         </Box>
 
