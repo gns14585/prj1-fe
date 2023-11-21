8dde6d7eabf0ad2303b4a2daaf22b4a55d231668
LeeJungHoon <gns14585@naver.com>
2023-11-21 16:16:15 +0900

    로그인 한 회원 별명 navbar에 출력

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,4 +1,4 @@
-import { Button, Flex, useToast } from "@chakra-ui/react";
+import { Box, Button, Flex, useToast } from "@chakra-ui/react";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useContext, useEffect } from "react";
@@ -44,6 +44,8 @@ export function NavBar() {
 
   return (
     <Flex>
+      {isAuthenticated() && <Box>{login.nickName}님</Box>}
+
       <Button onClick={() => navigate("/")}>
         <FontAwesomeIcon icon={faHouse} />
         {"\u00A0"}
